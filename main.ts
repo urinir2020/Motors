0cee5d20b521313c653fc4e9528aa188da069817
urinir2020 <112821482+urinir2020@users.noreply.github.com>
2022-09-04 21:37:31 +0300

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,19 @@
-SuperBit.MotorRunDual(
-SuperBit.enMotors.M1,
-255,
-SuperBit.enMotors.M1,
-255
-)
+input.onSound(DetectedSound.Loud, function () {
+    L_SPEED += 5
+    R_SPEED += 10
+})
+let L_SPEED = 0
+let R_SPEED = 0
 basic.forever(function () {
-    basic.showIcon(IconNames.Yes)
+    if (input.isGesture(Gesture.LogoUp)) {
+        SuperBit.MotorRunDual(
+        SuperBit.enMotors.M1,
+        L_SPEED,
+        SuperBit.enMotors.M3,
+        R_SPEED
+        )
+        basic.showNumber(R_SPEED)
+    } else {
+        SuperBit.MotorStopAll()
+    }
 })
